9eb1691e7decde3e4a3df59d227c8c68b210f801
mhmad wrekat <mhmmadwreekkat@gmail.com>
2022-02-12 23:59:35 +0200

    update

--- components/Nav.js
+++ components/Nav.js
@@ -13,26 +13,26 @@ const Nav = () => {
       </Head>
       <section className='' translate='no'>
         <nav class=" flex items-center justify-between flex-wrap bg-sky-800 lg:p-6 p-2">
+          <div class="flex items-center flex-shrink-0 text-white lg:mr-6 mr-0">
+            <Link href='/'>
+              <div className='flex items-center flex-shrink-0 mr-0 text-white'>
+              <img src='./assest/logoo.png'
+                  class="fill-current h-12 w-12 cursor-grab lg:mr-2 mr-0" />
+                  <span class="cursor-grab font-semibold text-xl tracking-tight lg:mx-6 mx-3">صوفاتك</span>
+              </div>
+            </Link>
+            <a href="https://www.facebook.com/صوفاتك-658378061026720/" data-tippy-content="facebook"
+              target='_blank'
+              className='cursor-grab hover:scale-110'>
+              <img src='./assest/facebook.png'
+                className='fill-current w-7 h-7' /></a>
+          </div>
           <a href="https://wa.me/+962795152466" data-tippy-content="WhatsApp">
             <button translate='no'
               class="cursor-grab bg-gradient-to-r from-amber-300 to-amber-600 ... animate-bounce text-white font-bold py-2 px-4 rounded-full">
               أطلب الآن
             </button>
           </a>
-          <div class="flex items-center flex-shrink-0 text-white lg:mr-6 mr-0">
-            <a href="https://www.facebook.com/صوفاتك-658378061026720/" data-tippy-content="facebook"
-              target='_blank'
-              className='pr-4 cursor-grab hover:scale-110'>
-              <img src='./assest/facebook.png'
-                className='fill-current w-7 h-7' /></a>
-            <Link href='/'>
-              <div className='flex items-center flex-shrink-0 mr-0 text-white lg:mr-5'>
-                  <span class="cursor-grab font-semibold text-xl tracking-tight lg:mr-6 mr-3">صوفاتك</span>
-                <img src='./assest/logoo.png'
-                  class="fill-current h-12 w-12 cursor-grab lg:mr-2 mr-0" />
-              </div>
-            </Link>
-          </div>
         </nav>
       </section>
       <div className='lg:pt-0'>
